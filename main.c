7bc5ac0d6926bf5fca0585796fdc57f28ccb4081
Nub4Fun22 <86065477+Nub4Fun22@users.noreply.github.com>
2025-03-31 21:02:58 +0300

    Add files via upload
    Added Unloading And Initialization of the Cards ( Currently , it's not working for now)

--- main.c
+++ main.c
@@ -7,12 +7,46 @@
 
 bool Start_Game = false;
 
+typedef struct {
+    char poza_carte[3000];
+    Texture2D texture_carti;
+    int cnt_carte;
+}Carte;
+
+void Initializare_Carti(Carte *set_carti)
+{
+    for(int i=2;i<12;i++)
+    {
+        sprintf(set_carti[i].poza_carte,"E:/Facultate/Tp_Proiect/My_Project/Cards/Card_%d.png",i);
+        Image image=LoadImage(set_carti[i].poza_carte);
+         if (image.data == NULL) {
+            printf("Failed to load image: %s\n", set_carti[i].poza_carte);
+            continue; // Skip this card if the image fails to load
+        }
+        set_carti[i].texture_carti=LoadTextureFromImage(image);
+        UnloadImage(image);
+         if (set_carti[i].texture_carti.id == 0) {
+            printf("Failed to load texture from image: %s\n", set_carti[i].poza_carte);
+            continue; // Skip this card if the texture fails to load
+         }
+    }
+}
+
+void Unload_Carti(Carte *set_carti)
+{
+    for (int i = 2; i < 12; i++)
+    {
+        UnloadTexture(set_carti[i].texture_carti); // Unload the texture
+    }
+}
 
 int main(void)
 {
+    Carte set_carti[12];
+    Initializare_Carti(set_carti);
     GuiLoadIcons("iconset.rgi",true);  
     SetTargetFPS(60);   ///Frames per second
-    InitWindow(Screen_Width,Screen_Height,"Blackjack");
+    InitWindow(Screen_Width,Screen_Height,"Blackjack");  // The Meniu Window
     while (!WindowShouldClose())
     {
         float button_X,button_Y;
@@ -36,12 +70,16 @@ int main(void)
             {
                 EndDrawing();
                 CloseWindow();
-                InitWindow(Screen_Width,Screen_Height,"Blackjack");
+                InitWindow(Screen_Width,Screen_Height,"Blackjack"); // The Window for the game itself
                 while (!WindowShouldClose())
                 {
+                    
                     BeginDrawing();
                     
                     ClearBackground(DARKGREEN);
+
+                            DrawTexture(set_carti[3].texture_carti,480,320,DARKGRAY);
+
 
                     EndDrawing();
 
@@ -53,4 +91,4 @@ int main(void)
     }
     CloseWindow();
     return 0;
-}
+}                         

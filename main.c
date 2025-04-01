d6c7dfac56e0ae51d8ad09b1a59846e7debacb0e
Nub4Fun22 <86065477+Nub4Fun22@users.noreply.github.com>
2025-04-01 21:32:03 +0300

    Add files via upload
    Added a working and functional button wich Draws a random card

--- main.c
+++ main.c
@@ -6,20 +6,21 @@
 #define Screen_Width 800   
 #define Screen_Height 450
 #define Numar_Carti 12    // TO NOT BE MODIFIED !!
+#define Numar_Maxim_Tragere_Carti 5 // TO NOT BE MODIFIED !!
 bool Start_Game = false;
 
 typedef struct {
     int afisare_carte; // contor afisare carte per frame
     char poza_carte[3000];
     Texture2D texture_carti;
-    int cnt_carte;
+    int val_carte;
 }Carte;
 
 void Initializare_Carti(Carte *set_carti)
 {
     for(int i=2;i<Numar_Carti;i++)
     {
-        set_carti[i].cnt_carte=2;
+        set_carti[i].val_carte=i;
         set_carti[i].afisare_carte=0;
         sprintf(set_carti[i].poza_carte,"E:/Facultate/Tp_Proiect/My_Project/Cards/Card_%d.png",i);
         Image image=LoadImage(set_carti[i].poza_carte);
@@ -49,7 +50,7 @@ void Unload_Carti(Carte *set_carti)
 int Trage_Carte(Carte *set_carti,float Center_X,float Center_Y,int counter)
 {
     int carte_index=rand()%Numar_Carti;
-    if(counter<5)
+    if(counter<Numar_Maxim_Tragere_Carti)
     {
         while(true)
         {
@@ -102,7 +103,7 @@ int main(void)
                     GuiSetFont(GetFontDefault());
 
                     int Card_Counter=0;
-
+                    int afisare_carduri_ordine[Numar_Maxim_Tragere_Carti]={0};
                 while (!WindowShouldClose())
                 {
                     BeginDrawing();
@@ -122,24 +123,18 @@ int main(void)
                             {
                                 set_carti[carte_trasa].afisare_carte++;
                                 printf("Nr de carti pentru %d este %d\n",carte_trasa,set_carti[carte_trasa].afisare_carte);
+                                afisare_carduri_ordine[Card_Counter]=carte_trasa;
                             }
                             Card_Counter++;
                         }
 
-
-                    for(int i=2;i<Numar_Carti;i++)//A se trata desenarea cartilor cum trebe per total
-                    {
-                        if(set_carti[i].afisare_carte!=0)
-                            {
-                                if(set_carti[i].afisare_carte==1)
-                                DrawTexture(set_carti[i].texture_carti,Center_X+((i-4)*40),Center_Y,WHITE);
-                                else
-                                {
-                                    DrawTexture(set_carti[i].texture_carti,Center_X+((i-4)*40),Center_Y,WHITE);
-                                    DrawTexture(set_carti[i].texture_carti,Center_X+((i+10-4)*40),Center_Y,WHITE); //A se trata desenarea cartilor cum trebe pt duble
-                                }
-                            }                          
-                    }
+                        for(int i=0;i<Card_Counter;i++)
+                        {
+                            int index=afisare_carduri_ordine[i];
+                            if(index!=0)
+                                if(set_carti[index].afisare_carte!=0)
+                                    DrawTexture(set_carti[index].texture_carti,Center_X+((i-4)*40),Center_Y,WHITE);
+                        }
                     EndDrawing();
 
                 }
